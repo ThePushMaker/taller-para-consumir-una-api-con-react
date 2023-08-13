8446faa9ae2537f5146d5f01e6a57a14c1a2c5c9
DSW <elmcwl@gmail.com>
2023-08-13 04:22:08 -0700

    add axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { ProductCard } from './components/ProductCard/ProductCard.jsx';
 import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
   // setProducts es la funcion que vamos a utilizar para poder cambiar el valor de nuestros productos
   const[products, setProducts] = useState([])
-
+  
   return (
     <div className="App">
      <h1>Lista de productos</h1>
