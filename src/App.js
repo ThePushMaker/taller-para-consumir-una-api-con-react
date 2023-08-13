d88c6b77a8704bd961c1abb1468d32cab62151ab
DSW <elmcwl@gmail.com>
2023-08-12 23:49:04 -0700

    ProductCard importado en app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import './components/ProductCard/ProductCard.jsx'
+import { ProductCard } from './components/ProductCard/ProductCard.jsx';
 
 function App() {
   return (
     <div className="App">
      <h1>Lista de productos</h1>
+      <ProductCard 
+        title="Producto prueba"
+        price="12.50"
+      />
     </div>
   );
 }
